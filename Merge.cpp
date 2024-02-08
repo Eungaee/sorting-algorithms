e0c194b7c9977c3ef37153b9b8a7a065bb470276
Eungaee <aidjiwon1004@gmail.com>
2024-02-09 01:23:17 +0900

    Merge 주석 정리

--- Merge.cpp
+++ Merge.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 
 using namespace std;
-
+// 마지막 분할은 Merge() 안에서 하는 것. (2개나 3개부터)
 void Merge(int *a, int left, int center, int right) {
-    int b[5], i = left, j = center + 1,
-              k{};  // 마지막 분할은 Merge() 안에서 하는 것. (2개나 3개부터)
-    while (
-        i <= center &&
-        j <= right) {  // 배열 왼쪽 오른쪽 나눠서 왼쪽의0과 오른쪽의0 부터 비교.
+    int b[1000], i = left, j = center + 1, k{};
+    // 배열 왼쪽 오른쪽 나눠서 왼쪽의0과 오른쪽의0 부터 비교.
+    while (i <= center && j <= right) {
         if (a[i] > a[j])
             b[k++] = a[j++];
         else
             b[k++] = a[i++];
     }
-    if (i <= center)
-        b[k] = a[i];  // 왼쪽 요소가 하나 더 많아서 위에서 처리 안됐을 때 b 제일
-                      // 뒤에 추가
-    if (j <= right) b[k] = a[j];  // 오른쪽 요소가 ..
-    do {                          // copy b to a.
+    // 왼쪽 요소나 오른쪽 요소가 하나 더 많아서 처리 안됐을 때 b 뒤에 추가
+    if (i <= center) b[k] = a[i];
+    if (j <= right) b[k] = a[j];
+    do {  // copy b to a.
         a[left + k] = b[k];
     } while (k--);  // k--를 나중에 실행해야 의도한대로 작동해서 do while 사용.
-    /* while (k >= 0) {    // Also OK.
+    // AlsoOK
+    /* while (k >= 0) {
         a[left + k] = b[k];
         k--
     } */
@@ -37,7 +35,7 @@ void MergeSort(int *a, int left, int right) {
 }
 
 int main() {
-    int a[5], n, left, right;
+    int a[1000], n, left, right;
     cin >> n;
     for (int i = 0; i < n; i++) cin >> a[i];
     MergeSort(a, 0, n - 1);
